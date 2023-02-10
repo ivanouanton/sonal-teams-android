e0dd5337ea320f9de85ec0c090e4bd0d32158890
Anton Ivanou <aivanov@remedypointsolutions.com>
2023-02-11 02:16:12 +0300

    Converted DeviceFragment and DeviceViewModel into kotlin

--- app/src/main/java/com/waveneuro/ui/dashboard/device/DeviceViewEvent.kt
+++ app/src/main/java/com/waveneuro/ui/dashboard/device/DeviceViewEvent.kt
@@ -3,14 +3,14 @@ package com.waveneuro.ui.dashboard.device
 import com.waveneuro.data.model.entity.BleDevice
 
 sealed class DeviceViewEvent {
-    class Start : DeviceViewEvent()
-    class LocateDevice : DeviceViewEvent()
-    class DeviceClicked(val bleDevice: BleDevice)  : DeviceViewEvent()
-    class Connected : DeviceViewEvent()
-    class Disconnected : DeviceViewEvent()
-    class Searched : DeviceViewEvent()
-    class NoDeviceFound : DeviceViewEvent()
-    class BackClicked : DeviceViewEvent()
-    class StartSessionClicked : DeviceViewEvent()
-    class LocateDeviceNextClicked : DeviceViewEvent()
+    object Start : DeviceViewEvent()
+    object LocateDevice : DeviceViewEvent()
+    data class DeviceClicked(val bleDevice: BleDevice)  : DeviceViewEvent()
+    object Connected : DeviceViewEvent()
+    object Disconnected : DeviceViewEvent()
+    object Searched : DeviceViewEvent()
+    object NoDeviceFound : DeviceViewEvent()
+    object BackClicked : DeviceViewEvent()
+    object StartSessionClicked : DeviceViewEvent()
+    object LocateDeviceNextClicked : DeviceViewEvent()
 }
