41c44684137739b0aa5f5a418cbc535554d9e40c
Anton Ivanou <aivanov@remedypointsolutions.com>
2023-02-21 14:37:36 +0300

    Added loader and low battery pop up on session screen

--- app/src/main/java/com/waveneuro/ui/session/session/SessionViewEffect.kt
+++ app/src/main/java/com/waveneuro/ui/session/session/SessionViewEffect.kt
@@ -3,4 +3,6 @@ package com.waveneuro.ui.session.session
 sealed class SessionViewEffect {
     object Back : SessionViewEffect()
     object InitializeBle : SessionViewEffect()
+    object ShowLoader : SessionViewEffect()
+    object HideLoader : SessionViewEffect()
 }
